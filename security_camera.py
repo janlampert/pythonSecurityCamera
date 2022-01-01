c49439fab421ba9c0692df4de2f04db4b5617619
Jan Lampert <83476259+janlampert@users.noreply.github.com>
2022-01-01 14:16:01 +0100

    Adding the main file

---
+++ security_camera.py
@@ -0,0 +1,74 @@
+import cv2
+import time
+import datetime
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+#import os
+
+cap = cv2.VideoCapture(1)
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+#Video Voraussetzungen
+framesize = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+#start recording:
+#out = cv2.VideoWriter("video.mp4", fourcc, 20.0, framesize)
+#stop recording:
+#out.release
+
+#prepare camera logic
+timer = 5
+recording = False
+recording_stopped_time = None
+timer_started = False
+
+#save video to google drive
+def saveToCloud(out):
+
+
+
+while True:
+    _, frame = cap.read()
+
+    #Gesicht(er) erkennen
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 3)
+
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x+width, y + height), (255, 0, 0), 3)
+
+    if len(faces) >0:
+        if recording:
+            timer_started = False
+
+        else:
+            date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            out = cv2.VideoWriter((date + ".mp4"), fourcc, 10.0, framesize)
+            recording = True
+            print ("Aufnahme erfolgreich gestartet!")
+
+    elif recording:
+        if timer_started:
+            if time.time() - recording_stopped_time >= timer:
+                    recording = False
+                    timer_started = False
+                    out.release()
+                    print("Aufnahme wurde abgebrochen!")
+        else:
+            timer_started = True
+            recording_stopped_time = time.time()
+
+    if recording:
+        out.write(frame)
+
+    cv2.imshow("Ueberwachungskamera", frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+out.release()
